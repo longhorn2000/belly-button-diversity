c5643e88cc799561e23a3faf614f764af9e01b41
longhorn2000 <80234800+longhorn2000@users.noreply.github.com>
2021-07-12 11:06:53 -0500

    add

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,24 @@ function optionChanged(selectednumber) {
       showmetadata(barfilteredmeta[0])
   })
 }
+function init() {
+  var dropdown = d3.select("#selDataset") //referencing back to html  and appending dropdown
+  d3.json("samples.json").then(function (data) {
+      data.names.forEach((people) => {
+          dropdown.append("option")
+              .text(people)
+              .property("value", people)
+      })
+      //line 17 starts looking at names list with ids. then looping through the people on list
+      var initialdata = data.samples[0]
+      console.log(data);
+      Bardata(initialdata)
+      Bubbledata(initialdata)
+      var metadata = data.metadata[0]
+      showmetadata(metadata)
+  });
+}
+
 
 
 
